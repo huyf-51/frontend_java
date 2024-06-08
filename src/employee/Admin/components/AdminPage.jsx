2ee0153d6a455afac0abcc09353fc99b7cb8340e
Huy <dgh2003lqm@gmail.com>
2024-06-08 22:43:24 +0700

    fix admin dashboard render

--- src/employee/Admin/components/AdminPage.jsx
+++ src/employee/Admin/components/AdminPage.jsx
@@ -3,7 +3,7 @@ import EmployeeList from './EmployeeList';
 
 const TeacherPage = () => {
     return (
-        <div className="mb-20">
+        <div className="pb-20">
             <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
                 <thead className="flex justify-center">
                     <tr>
